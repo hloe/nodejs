ebabbdccbd9d99fab96e6f97ba7956d7144b7927
Larysa Goroshanska <Larysa_Goroshanska@epam.com>
2020-09-23 16:51:19 +0300

    Add tast 5.1

--- hw3-7/rest/routers/router.js
+++ hw3-7/rest/routers/router.js
@@ -4,10 +4,13 @@ import UserService from './../services/user.js';
 import GroupService from './../services/group.js';
 import UserGroupService from './../services/userGroup.js';
 
+import logger from './logger.js';
+
 const router = express.Router();
 
 // GET all users
 router.get('/users', async (req, res) => {
+    logger('UserService.GetAllUsers');
     const users = await UserService.GetAllUsers();
 
     res.status(200).json(users);
@@ -15,6 +18,7 @@ router.get('/users', async (req, res) => {
 
 // GET user by id
 router.get('/users/:id', async (req, res) => {
+    logger('await UserService.GetUserById', { id: req.params.id });
     const found = await UserService.GetUserById(req.params.id);
 
     if (found) {
@@ -30,6 +34,7 @@ router.get('/users/list/:limit/:loginSubstring', async (req, res) => {
         res.status(400).send('limit must be more then 0');
     }
 
+    logger('UserService.GetAutoSuggestList', req.params);
     const resUsers = await UserService.GetAutoSuggestList(req.params);
     res.status(201).json(resUsers);
 });
@@ -37,17 +42,21 @@ router.get('/users/list/:limit/:loginSubstring', async (req, res) => {
 // CREATE and UPDATE user
 router.post('/users', validateSchema(userSchema), async (req, res) => {
     // UPDATE user
+    logger('UserService.GetUserById', { id: req.body.id });
     const currentUser = await UserService.GetUserById(req.body.id);
 
     if (currentUser) {
+        logger('UserService.UpdateUser', req.body);
         await UserService.UpdateUser(req.body);
         res.sendStatus(204);
     } else {
         // CREATE user
+        logger('UserService.CheckIfExists', { login: req.body.login });
         const isLoginUsed = await UserService.CheckIfExists(req.body.login);
         if (isLoginUsed) {
             res.status(400).send('User with such login exists already');
         } else {
+            logger('UserService.CreateUser', req.body);
             await UserService.CreateUser(req.body);
             res.sendStatus(204);
         }
@@ -57,9 +66,11 @@ router.post('/users', validateSchema(userSchema), async (req, res) => {
 // DELETE user
 router.delete('/users/:id', async (req, res) => {
     const { id } = req.params;
+    logger('UserService.GetUserById', { id });
     const deletedUser = await UserService.GetUserById(id);
 
     if (deletedUser) {
+        logger('UserService.DeleteUser', { id });
         await UserService.DeleteUser(id);
 
         res.sendStatus(204);
@@ -70,6 +81,7 @@ router.delete('/users/:id', async (req, res) => {
 
 // GET all groups
 router.get('/groups', async (req, res) => {
+    logger('GroupService.GetAllGroups');
     const users = await GroupService.GetAllGroups();
 
     res.status(200).json(users);
@@ -77,6 +89,7 @@ router.get('/groups', async (req, res) => {
 
 // GET group by id
 router.get('/groups/:id', async (req, res) => {
+    logger('GroupService.GetGroupById', { id: req.params.id });
     const found = await GroupService.GetGroupById(req.params.id);
 
     if (found) {
@@ -89,17 +102,21 @@ router.get('/groups/:id', async (req, res) => {
 // CREATE and UPDATE group
 router.post('/groups', validateSchema(groupSchema), async (req, res) => {
     // UPDATE group
+    logger('GroupService.GetGroupById', { id: req.params.id });
     const currentGroup = await GroupService.GetGroupById(req.body.id);
 
     if (currentGroup) {
+        logger('GroupService.UpdateGroup', req.body);
         await GroupService.UpdateGroup(req.body);
         res.sendStatus(204);
     } else {
         // CREATE group
+        logger('GroupService.CheckIfExists', { name: req.body.name });
         const isNameUsed = await GroupService.CheckIfExists(req.body.name);
         if (isNameUsed) {
             res.status(400).send('User with such login exists already');
         } else {
+            logger('GroupService.CreateGroup', req.body);
             await GroupService.CreateGroup(req.body);
             res.sendStatus(204);
         }
@@ -109,9 +126,11 @@ router.post('/groups', validateSchema(groupSchema), async (req, res) => {
 // DELETE group
 router.delete('/groups/:id', async (req, res) => {
     const { id } = req.params;
+    logger('GroupService.GetGroupById', { id });
     const deletedGroup = await GroupService.GetGroupById(id);
 
     if (deletedGroup) {
+        logger('GroupService.DeleteGroup', { id });
         await GroupService.DeleteGroup(id);
 
         res.sendStatus(204);
@@ -128,13 +147,16 @@ router.post('/groups/add-users', async (req, res) => {
         res.status(400).send('Users array can not be empty');
     }
 
+    logger('GroupService.GetGroupById', { groupId });
     const currentGroup = await GroupService.GetGroupById(groupId);
+    logger('UserGroupService.GetUsersById', { userIds });
     const users = await UserGroupService.GetUsersById(userIds);
     const userNotFound = users.some(user => !user);
 
     if (!currentGroup || userNotFound) {
         res.sendStatus(404);
     } else {
+        logger('UserGroupService.AddUsersToGroup', { userIds });
         await UserGroupService.AddUsersToGroup(groupId, userIds);
         res.sendStatus(204);
     }
