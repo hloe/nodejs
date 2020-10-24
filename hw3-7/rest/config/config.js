db5d2409fb61d1d98b020ac47f0d65829c896657
Larysa Goroshanska <Larysa_Goroshanska@epam.com>
2020-10-25 01:48:30 +0300

    Add task 7.2

--- hw3-7/rest/config/config.js
+++ hw3-7/rest/config/config.js
@@ -1,6 +1,10 @@
-const configUrl = 'postgres://xfppzfva:Jlbv-sqXue75vp_F1SkS3O2AcN_HxAeP@otto.db.elephantsql.com:5432/xfppzfva';
-const corsUrl = 8100;
-const port = 3000;
-const secret = 'secret_for_roaling_getting_token';
+import dotenv from 'dotenv';
+// eslint-disable-next-line no-unused-vars
+const config = dotenv.config();
+
+const configUrl = process.env.CONFIG_URL;
+const corsUrl = process.env.CORS_URL;
+const port = process.env.PORT;
+const secret = process.env.SECRET;
 
 export { configUrl, corsUrl, port, secret };
